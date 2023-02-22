a75c4c3efbb53ab37b2b125871527060a1354441
agmas <Maxlion360@gmail.com>
2023-02-22 23:25:18 +0400

    a

--- src/main/java/agmas/kingsbutbad/listeners/AsyncPlayerChatEventListener.java
+++ src/main/java/agmas/kingsbutbad/listeners/AsyncPlayerChatEventListener.java
@@ -1,17 +1,9 @@
 package agmas.kingsbutbad.listeners;
 
-import agmas.kingsbutbad.KingsButBad;
-import agmas.kingsbutbad.utils.Role;
-import agmas.kingsbutbad.utils.RoleManager;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 public class AsyncPlayerChatEventListener implements Listener {
     @EventHandler
